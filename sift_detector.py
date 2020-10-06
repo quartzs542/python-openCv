44693a39d1c009ede83cccf313be4c3b68f4aa76
Animesh Gupta <56643388+g-animesh02@users.noreply.github.com>
2020-10-06 09:31:47 +0530

    Update sift_detector.py

--- sift_detector.py
+++ sift_detector.py
@@ -1,8 +1,12 @@
 import cv2
+#SIFT and SURF implementations are no longer included in the OpenCV 3 library so
+#use lower versions of OpenCV
+#pip install opencv-contrib-python==3.4.2.17
 
 img = cv2.imread('IMAGE_NAME.jpg')
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#sift = cv2.xfeatures2d.SIFT_create()
 sift = cv2.SIFT_create()
 kp, des = sift.detectAndCompute(gray_img, None)
 
