4f2464e47510b0d5e9cdbe255710973dfffa4c4d
Prady2001 <55875818+Prady2001@users.noreply.github.com>
2020-10-18 13:58:52 +0530

    To search for a image
    This functions helps in template matching in a particular region of the image or screen.

---
+++ image-search.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+def imagesearcharea(image, x1, y1, x2, y2, precision=0.8, im=None):
+    if im is None:
+        im = region_grabber(region=(x1, y1, x2, y2))
+        if is_retina:
+            im.thumbnail((round(im.size[0] * 0.5), round(im.size[1] * 0.5)))
+        # im.save('testarea.png') usefull for debugging purposes, this will save the captured region as "testarea.png"
+
+    img_rgb = np.array(im)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread(image, 0)
+
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    if max_val < precision:
+        return [-1, -1]
+    return max_loc
